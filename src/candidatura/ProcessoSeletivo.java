1b78d2c6b13c2d3cde12ca3179a26f0887501795
João Pedro Silva Cunha <jpcunha5@gmail.com>
2025-06-27 04:38:20 -0300

    :sparkles: Metodo p/ seleção de candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,18 +1,49 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
     }
 
-    public static void analisarCandidato(double salarioPretensao){
+    // Função auxiliar para gerar um salário de pretensão aleatório
+    static double pretensaoSalarial(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void selecaoCandidatos() {
+        String[] candidatos = {
+            "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO",
+            "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"
+        };
+
+        int selecionados = 0;
+        int atual = 0;
         double salarioBase = 2000.0;
 
-        if(salarioBase > salarioPretensao)
+        while(selecionados < 5 && atual < candidatos.length) {
+            String candidato = candidatos[atual];
+            System.out.println("Candidato: " + candidato);
+
+            double salarioPretensao = pretensaoSalarial();
+            System.out.println("Salario Pretensao: " + String.format("%.2f", salarioPretensao));
+
+            if(salarioBase >= salarioPretensao){
+                selecionados++;
+                System.out.println("Candidato Selecionado: " + candidato);
+            }
+
+            atual++;
+        }
+    }
+
+    static void analisarCandidato(double salarioPretensao) {
+        double salarioBase = 2000.0;
+
+        if (salarioBase > salarioPretensao)
             System.out.println("LIGAR PARA O CANDIDATO");
-        else if(salarioBase == salarioPretensao)
+        else if (salarioBase == salarioPretensao)
             System.out.println("LIGAR PARA O CANDIDATO COM CONTRAPROPOSTA");
         else
             System.out.println("AGUARDANDO RESULTADO DOS OUTROS CANDIDATOS");
